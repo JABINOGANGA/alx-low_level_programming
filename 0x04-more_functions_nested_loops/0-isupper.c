d95edb98e524cd22677af2c7ae718e915e54cd49
root <jabinoganga.jo@gmail.com>
2022-09-15 19:55:35 -0700

    Function X 2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 /**
  * _isupper -Excecution of code begins here
  *
- * @l: character
+ * @c: character
  *
  * Return: 1 if true  and -1 if false
  */
 
-int _isupper(int l)
+int _isupper(int c)
 {
 
-	if (l >= 'A' && l <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
