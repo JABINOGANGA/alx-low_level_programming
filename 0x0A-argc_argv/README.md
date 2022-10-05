28f6c9f9e17b4b7bf9872a67f90ae27d6711dbc8
root <jabinoganga.jo@gmail.com>
2022-10-04 17:19:52 -0700

    Arguments in command line with C

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+# C - argc, argv
+
+## Tasks - programs printing names, number of arguments passed on it, arguments received, multiplies numbers, adds two positive numbers
+## Resources - Youtube, stackoverflow, alx content
+
+## Thoughts - Quite Indulging
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,22 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * _putchar - writes the character c to stdout
+ * Function prototypes
+ */
+
+int _putchar(char);
+
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+#endif /* _SAMPLE_HEADER_H_ */
