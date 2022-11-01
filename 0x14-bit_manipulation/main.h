7a9a1a9ff25152407c144a1861a9ddabf8ed8773
root <jabinoganga.jo@gmail.com>
2022-10-31 20:02:53 -0700

    Hello

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
